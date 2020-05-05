1ab3f641634ec63ed7bd40d328c087a358fff21d
Thomas John Helin <thomashelin@yahoo.com>
2020-05-05 16:20:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,30 +24,30 @@ class Song
   end
 
   def self.create_by_name(name)
-    song = self.create
+    song = self.new
     song.name = name
+    @@all << song
     song
   end
 
   def self.find_by_name
-    @@all.each do |song|
-      if song.name == name
-        return song
-      end
-    end
+    @@all.detect {|song| song.name == name}
   end
 
   def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create_by_name(name)
-
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
   end
 
   def self.alphabetical
-    @@all.sort_by{|song| song.name}
+    self.all.sort_by! {|song| song.name}
   end
 
-  def self.new_from_filename
-    row = filename
+  def self.new_from_filename(song_with_format)
+    row = song_with_format
     data = row.split(" - ")
     artist_name = data[0]
     song_name = data[1].gsub(".mp3", "")
@@ -57,13 +57,13 @@ class Song
     song
   end
 
-  def self.create_from_filename(filename)
+  def self.create_from_filename(song_with_format)
     song = self.new_from_filename(filename)
     song.save
     song
   end
 
   def self.destroy_all
-
+    self.all.clear
   end
 end
